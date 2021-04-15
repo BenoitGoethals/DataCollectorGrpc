7cb88bf22cc6083c7456e7fa014b6d0ada8163c3
benoitgoethals <benoit.goethals@mil.be>
2021-04-15 09:29:11 +0200

    update

---
+++ DataAnalyser.Grpc/Services/IntelService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DataAnalyser.Service;
+using Grpc.Core;
+
+namespace DataAnalyser.Grpc.Services
+{
+    public class IntelService : IntelData.IntelDataBase
+    {
+        private readonly IDataService _dataService;
+
+        public IntelService(IDataService dataService)
+        {
+            _dataService = dataService;
+        }
+
+        public override async Task<IntelDataCollection> GetIntel(Keyword request, ServerCallContext context)
+        {
+            var list = new List<Intel>();
+            var ret = await _dataService.Collect(request.Name);
+            ret.ForEach(i => list.Add(new Intel()));
+            var reply = new IntelDataCollection();
+            return await Task.FromResult(reply);
+        }
+    }
+}
